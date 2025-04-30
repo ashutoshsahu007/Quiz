73a007e50ab5b3ae6264aef587d1f7b0583d35f5
ashutosh sahu <ppaskumar@gmail.com>
2025-04-30 20:51:08 +0530

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,64 +4,20 @@ const Header = () => {
   return (
     <div className="bg-[#461f7f]">
       <header class="shadow-md">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-10">
           <div class="flex justify-between h-16 items-center">
-            {/* <!-- Logo --> */}
             <div class="flex-shrink-0 text-xl font-bold text-blue-600">
               <img
                 src="logo_light.png"
                 alt="h"
-                class="w-34 inline-block mr-2"
+                class="w-38 inline-block mr-2"
               />
             </div>
-
-            {/* <!-- Hamburger Button (mobile) --> */}
-            <div class="md:hidden">
-              <button id="menu-toggle" class="text-gray-700 focus:outline-none">
-                <svg
-                  class="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
-            </div>
-
-            {/* <!-- Navigation Links --> */}
-            <nav class="hidden md:flex space-x-4">
-              <a
-                href="#"
-                class="text-white hover:text-blue-600 font-black text-2xl"
-              >
-                Ashutosh Kumar
-              </a>
-              <a
-                href="#"
-                class="text-white hover:text-blue-600 border-1 border-white p-2"
-              >
-                <span>
-                  <img src="coin.png" alt="image not found" className="w-4" />
-                </span>
-              </a>
-            </nav>
+            <span class="text-white hover:text-blue-600 border-1 border-white py-0.5 px-3 rounded-2xl flex items-center gap-2">
+              <img src="coin.png" alt="image not found" className="w-5" />
+              <span className="font-bold text-xl text-amber-300">44</span>
+            </span>
           </div>
-        </div>
-
-        {/* <!-- Mobile Menu --> */}
-        <div id="mobile-menu" class="md:hidden hidden px-4 pb-4">
-          <a href="#" class="block py-2 text-gray-700 hover:text-blue-600">
-            Home
-          </a>
-          <a href="#" class="block py-2 text-gray-700 hover:text-blue-600">
-            About
-          </a>
         </div>
       </header>
     </div>
